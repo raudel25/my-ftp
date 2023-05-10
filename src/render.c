2b1aef50d9cd8535cdef09b51be89145ff2f1bec
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-05-10 12:51:16 -0400

    alphabetic order

--- src/render.c
+++ src/render.c
@@ -14,18 +14,18 @@
 #define MAX_SIZE_BUFFER 10240
 #define HTTP_HTML "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
 
-void build_table_name(char *html_response, char *path, char *root_path, struct dirent *dir) {
+void build_table_name(char *html_response, char *path, char *root_path, char *file) {
     strcat(html_response, "<a href=\"");
 
     char *redirect = path_server_to_browser(path, root_path);
     strcat(html_response, redirect);
 
     strcat(html_response, "/");
-    strcat(html_response, dir->d_name);
+    strcat(html_response, file);
     free(redirect);
 
     strcat(html_response, "\">");
-    strcat(html_response, dir->d_name);
+    strcat(html_response, file);
 
     strcat(html_response, "</a>");
 }
@@ -71,24 +71,46 @@ void build_table_last_date(char *html_response, struct stat st) {
     strcat(html_response, aux);
 }
 
+int compare(const void *a, const void *b) {
+    const char **ia = (const char **) a;
+    const char **ib = (const char **) b;
+    return strcmp(*ia, *ib);
+}
+
 char *build_table(DIR *d, char *path, char *root_path) {
     char *html_response = (char *) malloc(MAX_SIZE_BUFFER);
     strcpy(html_response, "<html><body><table><tr><th>Name</th><th>Size</th><th>Last Date</th></tr>");
 
     struct dirent *dir;
     struct stat st;
+    int capacity = 32;
+    int count = 0;
+    char **files = (char **) malloc(capacity * sizeof(char *));
 
     while ((dir = readdir(d)) != NULL) {
-        char aux_path[strlen(path) + strlen(dir->d_name) + 1];
+        if (count == capacity) {
+            capacity *= 2;
+            files = realloc(files, capacity * sizeof(char *));
+        }
+        files[count] = malloc(strlen(dir->d_name) + 1);
+        strcpy(files[count], dir->d_name);
+        count++;
+    }
+
+    qsort(files, count, sizeof(char *), compare);
+
+    for (int i = 0; i < count; i++) {
+        char aux_path[strlen(path) + strlen(files[i]) + 1];
+
         strcpy(aux_path, path);
         strcat(aux_path, "/");
-        strcat(aux_path, dir->d_name);
+        strcat(aux_path, files[i]);
 
         stat(aux_path, &st);
 
-        strcat(html_response, "<tr><th>");
+        strcat(html_response, "<tr><th style=\"text-align: left;\">");
 
-        build_table_name(html_response, path, root_path, dir);
+        build_table_name(html_response, path, root_path, files[i]);
 
         strcat(html_response, "</th><th>");
 
@@ -102,6 +124,7 @@ char *build_table(DIR *d, char *path, char *root_path) {
     }
 
     strcat(html_response, "</table></body></html>");
+    free(files);
 
     return html_response;
 }

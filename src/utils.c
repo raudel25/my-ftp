5740ce4362bf2ca9fad62941322be4fe63499b99
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-05-10 03:54:49 -0400

    spaces

--- src/utils.c
+++ src/utils.c
@@ -9,16 +9,38 @@
 #define MAX_SIZE_BUFFER 1024
 #define TOK_DELIM " \t\r\n\a"
 
-char *path_browser_to_server(char *path, char *root_path) {
+char *spaces(char *path) {
     int len = (int) strlen(path);
+    char *new_path = (char *) malloc(MAX_SIZE_BUFFER);
+    int j = 0;
+
+    for (int i = 0; i < len; i++) {
+        if (i <= len - 3 && path[i] == '%' && path[i + 1] == '2' && path[i + 2] == '0') {
+            new_path[j++] = ' ';
+            i += 2;
+            continue;
+        }
+
+        new_path[j++] = path[i];
+    }
+    new_path[j] = 0;
+
+    return new_path;
+}
+
+char *path_browser_to_server(char *path, char *root_path) {
+    char *spaces_path = spaces(path);
+
+    int len = (int) strlen(spaces_path);
     int len_root_path = (int) strlen(root_path);
 
     char *new_path = (char *) malloc(MAX_SIZE_BUFFER);
     strcpy(new_path, root_path);
-    strcat(new_path, path);
+    strcat(new_path, spaces_path);
 
-    if (new_path[len + len_root_path - 1] == '/') new_path[len +len_root_path - 1] = 0;
+    if (new_path[len + len_root_path - 1] == '/') new_path[len + len_root_path - 1] = 0;
 
+    free(spaces_path);
     return new_path;
 }
 
